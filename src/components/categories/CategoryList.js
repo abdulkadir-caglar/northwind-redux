1803576cf03c92f1c172d7a2764a0c36300a600c
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-22 21:10:21 +0300

    getCategories adn changeCategories added to category list with redux thunk

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
-export default class CategoryList extends Component {
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux';
+import * as categoryActions from "../../redux/actions/categoryActions"
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+class CategoryList extends Component {
+    componentDidMount(){
+        this.props.actions.getCategories();
+    }
+
     render() {
+        console.log("Categories:", this.props.categories);
+        console.log("Category Name: " + this.props.currentCategory.categoryName)
         return (
             <div>
-                <h3>Categories</h3>
+                <h3>Categories {this.props.categories.length}</h3>
+                <ListGroup>
+                    {this.props.categories.map(category => (
+                        <ListGroupItem onClick={this.props.actions.changeCategory(category)} key= {category.id}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+                <h5>Selected Category: {this.props.currentCategory.categoryName}</h5>
             </div>
         );
     }
 }
+
+function mapStateToProps(state){
+    return {
+        currentCategory:state.changeCategoryReducer,
+        categories:state.categoryListReducer
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return{
+        actions:{
+            getCategories: bindActionCreators(categoryActions.getCategories, dispatch),
+            changeCategory: bindActionCreators(categoryActions.changeCategory, dispatch)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
