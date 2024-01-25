c880dfcf7407ec7a04152c69a0348426894a7783
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-25 17:00:07 +0300

    NotFound componenet added and bugs fixed

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import { Route, Routes } from "react-router-dom";
 import CartDetail from "../cart/CartDetail";
 import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
+import NotFound from "../common/NotFound";
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         <Route exact path="/" element={<Dashboard/>}/>
         <Route exact path="/product" element={<Dashboard/>}/>
         <Route exact path="/cart" element={<CartDetail/>}/>
+        <Route path="/saveproduct" element={<AddOrUpdateProduct/>}/>
         <Route path="/saveproduct/:productId" element={<AddOrUpdateProduct/>}/>
+        <Route element={<NotFound/>}/>
       </Routes>
     </Container>
   );
