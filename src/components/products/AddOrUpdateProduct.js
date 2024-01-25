b77c59f168758ca771053b34c4ab12e6a0af723a
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-25 16:33:14 +0300

    fixed for AddOrUpdateProduct

--- src/components/products/AddOrUpdateProduct.js
+++ src/components/products/AddOrUpdateProduct.js
@@ -18,7 +18,6 @@ function AddOrUpdateProduct({ products,
         if (categories.length === 0) {
             getCategories();
         }
-
         setProduct({ ...props.product })
     }, [props.product]);
 
@@ -28,22 +27,22 @@ function AddOrUpdateProduct({ products,
             ...previousProduct,
             [name]: name === "categoryId" ? parseInt(value, 10) : value
         }));
-
-        function handleSave(event){
-
-            event.preventDefault();
-            saveProduct(product).then(() => history.push("/"));
-
-            return(
-                <ProductDetail 
-                product={product}
-                categories={categories}
-                onChange={handleChange}
-                onSave={handleSave}
-                />
-            )
-        }
     }
+
+    function handleSave(event){
+
+        event.preventDefault();
+        saveProduct(product).then(() => history.push("/"));
+    }
+
+    return(
+        <ProductDetail 
+        product={product}
+        categories={categories}
+        onChange={handleChange}
+        onSave={handleSave}
+        />
+    )
 }
 
 export function getProductById(products, productId){
@@ -52,15 +51,15 @@ export function getProductById(products, productId){
 }
 
 function mapStateToProps(state, ownProps){
-    const productId = ownProps.match.params.productId;
+    const productId = ownProps.productId;
 
-    const product = productId && state.productReducer.length > 0
-    ?getProductById(state.productReducer, productId)
+    const product = productId && state.productListReducer.length > 0
+    ?getProductById(state.productListReducer, productId)
     :{}
     return{
         product,
-        products : state.productReducer,
-        categories : state.categoryReducer
+        products : state.productListReducer,
+        categories : state.categoryListReducer
     }
 }
 
